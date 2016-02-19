c19db647ad7fa3a78dd043b36061d5b09b92fdd8
youdingzhiliqiang <18234034915@163.com>
2016-02-19 16:55:38 +0800

    测试

--- Study/Study/AppDelegate.m
+++ Study/Study/AppDelegate.m
@@ -17,6 +17,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    NSLog(@"123456");
+    
     return YES;
 }
 
