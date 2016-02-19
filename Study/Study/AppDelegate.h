ecf04874ec6e934737f983c79b77ab6559d3365d
youdingzhiliqiang <18234034915@163.com>
2016-02-19 16:52:06 +0800

    添加xcode程序

---
+++ Study/Study/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Study
+//
+//  Created by 李强 on 16/2/19.
+//  Copyright © 2016年 李强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
